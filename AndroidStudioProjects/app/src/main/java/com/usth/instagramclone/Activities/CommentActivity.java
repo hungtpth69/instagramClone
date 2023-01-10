a459db55634e7256fda71d1bc5a36b5bb745c4b8
Trần Duy Hưng <76862816+hungtpth69@users.noreply.github.com>
2023-01-10 17:16:42 +0700

    update

--- AndroidStudioProjects/app/src/main/java/com/usth/instagramclone/Activities/CommentActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/usth/instagramclone/Activities/CommentActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import com.usth.instagramclone.Fragments.HomeFragment;
+import com.usth.instagramclone.R;
 
 
 
@@ -13,20 +15,25 @@ public class CommentActivity extends AppCompatActivity {
 
     ImageView back;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout.activity_comment);
+        setContentView(R.layout.activity_comment);
 
         back = findViewById(R.id.back);
+
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CommentActivity.this, MainActivity.class);
-                startActivity(intent);
+                Intent intentComment = new Intent(CommentActivity.this, HomeFragment.class);
+                startActivity(intentComment);
             }
         });
+
     }
+
 }
 
 
